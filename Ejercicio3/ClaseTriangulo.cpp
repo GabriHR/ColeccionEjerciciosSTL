9f141bf87c75965a16ce3dc80d3cec2f6a294db8
GabriHR <ghernrod@myuax.com>
2023-11-15 19:56:00 +0100

    Ejercicio 3

---
+++ Ejercicio3/ClaseTriangulo.cpp
@@ -0,0 +1,21 @@
+
+#include <cmath>
+namespace Geometria {
+    class Triangulo {
+    private:
+        double base;
+        double altura;
+
+    public:
+        Triangulo(double b, double h) : base(b), altura(h) {}
+
+        double calcularArea() {
+            return (base * altura) / 2;
+        }
+
+
+        double calcularPerimetro() {
+            return base + altura + sqrt(base * base + altura * altura);
+        }
+    };
+}
